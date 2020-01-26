eee2d60bb5954402ff2a132d687596aea1baa6f6
garyxirapper <garyxirapper@aliyun.com>
2020-01-26 17:54:02 +0800

    完成订单管理功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,11 +37,13 @@ import {
   Step,
   CheckboxGroup,
   Checkbox,
-  Upload
+  Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
-import Timeline from './timeline/index.js'
-import TimelineItem from './timeline-item/index.js'
+/* import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js' */
 
 Vue.use(Button)
 Vue.use(Form)
